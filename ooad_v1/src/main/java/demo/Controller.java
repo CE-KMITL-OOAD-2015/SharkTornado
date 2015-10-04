a729bd223ed1dc863bfc8b7f656e81f5a991ea07
jwinut <nut.winut@gmail.com>
2015-10-04 10:01:49 +0700

    Include MongoDB

---
+++ ooad_v1/src/main/java/demo/Controller.java
@@ -0,0 +1,12 @@
+package demo;
+
+import com.mongodb.MongoClient;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Nut on 10/4/2015.
+ */
+@RestController
+public class Controller {
+    MongoClient
+}
