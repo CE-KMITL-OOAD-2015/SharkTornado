7282b7c123cdbb3a2bf90de9e6e785dbdd453e60
jwinut <nut.winut@gmail.com>
2015-10-14 14:12:22 +0700

    plan ขั้นต้นเสร็จแล้ว

--- ooad_1.3/src/main/java/th/ac/kmitl/ce/ooad/MainController.java
+++ ooad_1.3/src/main/java/th/ac/kmitl/ce/ooad/MainController.java
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class MainController {
 
-    @RequestMapping(value = "/addUser/{username}/{name}", params = {"e", "p", "i"})
+    @RequestMapping(value = "/addUser/{username}/{name}", params = {"email", "password", "imgLocation"})
     @ResponseBody
-    public boolean addUser(@PathVariable String username,@RequestParam("p") String passphrase,
-                           @PathVariable String name, @RequestParam("e") String email, @RequestParam("i") String imgLoc){
+    public boolean addUser(@PathVariable String username,@RequestParam("password") String passphrase,
+                           @PathVariable String name, @RequestParam("email") String email, @RequestParam("imgLocation") String imgLoc){
         System.out.println("Add " + username);
         String tmp = UserModel.getInstance().addUser(username, passphrase, name, email, imgLoc);
         if(tmp != null)System.out.println("Add " + username + " successfully.");
@@ -18,9 +18,9 @@ public class MainController {
         return tmp != null;
     }
 
-    @RequestMapping(value = "/login/{username}", params = "p")
+    @RequestMapping(value = "/login/{username}", params = "password")
     @ResponseBody
-    public boolean loginUser(@PathVariable String username, @RequestParam("p") String passphrase){
+    public boolean loginUser(@PathVariable String username, @RequestParam("password") String passphrase){
         return UserModel.getInstance().authenUser(username, passphrase);
     }
 
@@ -30,19 +30,19 @@ public class MainController {
         return UserModel.getInstance().isExist(username);
     }
 
-    @RequestMapping(value = "/update/name/{usrname}", params = {"name", "pwd"})
-    public boolean updateName(@PathVariable String usrname, @RequestParam("name") String name, @RequestParam("pwd") String pwd){
-        if(UserModel.authenUser(usrname, pwd)){
-            UserModel.getInstance().updateName(usrname, name);
+    @RequestMapping(value = "/update/name/{username}", params = {"name", "password"})
+    public boolean updateName(@PathVariable String username, @RequestParam("name") String name, @RequestParam("password") String pwd){
+        if(UserModel.getInstance().authenUser(username, pwd)){
+            UserModel.getInstance().updateName(username, name);
         }
         return false;
     }
 
-    @RequestMapping(value = "/update/pwd/{usrname}", params = {"pwd", "newpwd"})
+    @RequestMapping(value = "/update/password/{username}", params = {"password", "newpassword"})
     @ResponseBody
-    public boolean updatePwd(@PathVariable String usrname, @RequestParam("pwd") String pwd, @RequestParam("newpwd") String passphrase){
-        if(UserModel.authenUser(usrname, pwd)){
-            UserModel.getInstance().updatePwd(usrname, passphrase);
+    public boolean updatePwd(@PathVariable String username, @RequestParam("password") String pwd, @RequestParam("newpassword") String passphrase){
+        if(UserModel.getInstance().authenUser(username, pwd)){
+            UserModel.getInstance().updatePwd(username, passphrase);
             return true;
         }
         return false;
